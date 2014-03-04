01a5e31292410ce91ffbe919c606a43fb2d38735
AP7889 <alexpanka@Alex-Pankas-MacBook.local>
2014-03-04 15:50:21 -0500

    HW5

--- HW5_3.m
+++ HW5_3.m
@@ -1,8 +1,10 @@
 clear;
 
+for i=1:100
 x = chi2rnd(10,[1 10]);
 y = 3*chi2rnd(10,[1 10]);
-[h,p,ks2stat] = kstest2(x,y);
+[h(i),p(i),ks2stat(i)] = kstest2(x,y);
+end
 
 figure(1)
 ecdf(x)
@@ -11,7 +13,13 @@ ecdf(y)
 title('Empirical Cumulative Distribution Function')
 hold off
 
-ks2stat
-1/p
+figure(2)
+subplot(3,1,1); hist(h), title('h')
+subplot(3,1,2); hist(p), title('p')
+subplot(3,1,3); hist(ks2stat), title('ks2stat')
+
+fprintf('mean k-s statistic %.3f \n',mean(ks2stat))
+fprintf('Number of tries before you cannot reject null hypothesis: %.f \n',1/mean(p))
+
 
 kstat = .368;
